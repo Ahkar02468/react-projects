4cddf406e13403f30eda9f7d22a7bed748812ed2
Ahkar02468 <arkarshwebaw@gmail.com>
2025-04-27 05:38:28 +0700

    Refactor React Quiz application structure and components
    - Replaced the existing module structure with a new components structure.
    - Implemented a reducer for managing application state (loading, error, ready).
    - Added new components: Main, Loader, Error, and StartScreen.
    - Removed the old DateCounter component and re-implemented it in the new structure.
    - Updated the data fetching logic to retrieve questions from a local server.
    - Deleted the old questions.json file and added a new one in the data directory.

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,10 +1,48 @@
-import Header from "./modules/Header.jsx";
-import DateCounter from "./modules/DateCounter.jsx";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Loader from "./components/Loader.jsx";
+import Error from "./components/Error.jsx";
+import StartScreen from "./components/StartScreen.jsx";
+
+const initialState = {
+  questions: [],
+  //loading, error, ready, active, finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
+    <div className="app">
       <Header />
-      <DateCounter />
+      <Main>
+        {state.status === "loading" && <Loader />}
+        {state.status === "error" && <Error />}
+        {state.status === "ready" && <StartScreen />}
+      </Main>
     </div>
   );
 }
